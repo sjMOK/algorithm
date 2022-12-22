ad6cf1d565fc5d4744328443d870b11679e4e9fa
sjMOK <mokseungju@naver.com>
2022-12-22 10:22:41 +0900

    leetcode 21: Merge Two Sorted Lists

---
+++ leetcode/21.py
@@ -0,0 +1,33 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def merge_two_lists(list1, list2):
+    head = tail = ListNode()
+
+    while list1 and list2:
+        if list1.val < list2.val:
+            tail.next = list1
+            tail = list1
+            list1 = list1.next
+        else:
+            tail.next = list2
+            tail = list2
+            list2 = list2.next
+
+        tail.next = None
+
+    while list1:
+        tail.next = list1
+        tail = list1
+        list1 = list1.next
+        tail.next = None
+
+    while list2:
+        tail.next = list2
+        tail = list2
+        list2 = list2.next
+        tail.next = None
+
+    return head.next
