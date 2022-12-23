a65b6f3390c78e715dba6f2a7096a3c986f567b6
sjMOK <mokseungju@naver.com>
2022-12-23 17:10:05 +0900

    leetcode 316: Remove Duplicate Letters

---
+++ leetcode/316.py
@@ -0,0 +1,13 @@
+def remove_duplicate_letters(s):
+    stack = []
+
+    for i, char in enumerate(s):
+        if char not in stack:
+            while stack and stack[-1] in s[i:] and char < stack[-1]:
+                stack.pop()
+            
+            stack.append(char)
+
+    return ''.join(stack)
+
+print(remove_duplicate_letters('abacb'))
