92df4b4e0ef69867ba626bdc039155fffbab2f4d
sjMOK <mokseungju@naver.com>
2023-01-02 19:28:12 +0900

    improve using deque

--- leetcode/125.py
+++ leetcode/125.py
@@ -1,14 +1,13 @@
+import collections
+
 def is_palindrome(s):
-    converted_s = []
+    deque = collections.deque()
     for char in s:
         if char.isalnum():
-            converted_s.append(char.lower())
+            deque.append(char.lower())
 
-    i = 0
-    while i < len(converted_s) // 2:
-        if converted_s[i] != converted_s[len(converted_s) - 1 - i]:
+    while len(deque) > 1:
+        if deque.popleft() != deque.pop():
             return False
-
-        i += 1
 
     return True
